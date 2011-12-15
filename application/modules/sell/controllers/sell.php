4bcf60af77332bfd76cc8df8940ca45e984446b8
Phatograph <phatograph@gmail.com>
2011-12-15 22:27:30 +0700

    Sell frontend

--- application/modules/sell/controllers/sell.php
+++ application/modules/sell/controllers/sell.php
@@ -7,7 +7,7 @@ class Sell extends MX_Controller {
   }
   
 	public function index() {
-	  $this->db->select('s.id, s.name, si.id as imgid, si.name as imgname');
+	  $this->db->select('s.id, s.name, s.*, si.id as imgid, si.name as imgname');
 	  $this->db->from('sell s');
     $this->db->join('sell_image si', 'si.sell_id = s.id', 'left');
 	  $query = $this->db->get()->result();
@@ -17,6 +17,11 @@ class Sell extends MX_Controller {
 	  foreach($query as $m) {
 	    $data['machines'][$m->id]->id = $m->id;
 	    $data['machines'][$m->id]->name = $m->name;
+	    $data['machines'][$m->id]->brand = $m->brand;
+	    $data['machines'][$m->id]->serial = $m->serial;
+	    $data['machines'][$m->id]->hour = $m->hour;
+	    $data['machines'][$m->id]->price = $m->price;
+	    $data['machines'][$m->id]->note = $m->note;
 	    if($m->imgname) {
 	      $data['machines'][$m->id]->img[$m->imgid]->imgid = $m->imgid;
 	      $data['machines'][$m->id]->img[$m->imgid]->imgname = $m->imgname;
--- application/modules/sell/views/index.php
+++ application/modules/sell/views/index.php
@@ -1,12 +1,81 @@
-<ul>
-  <?php foreach($machines as $m) : ?>
-    <li><?= $m->name ?></li>
-    <?php if(isset($m->img)) : ?>
-      <ul>
-        <?php foreach($m->img as $i) : ?>
-          <li><img src="<?= base_url();?>images/uploaded/thumb_120x120/<?= $i->imgname ?>" alt="<?= $i->imgid ?>" /></li>
-        <?php endforeach; ?>
-      </ul>
-    <?php endif; ?>
-  <?php endforeach; ?>
-</ul>
+<div class="infoWrapper clearfix">
+  <div class="infoSidebar">
+    <div class="searchBox">
+      <?= form_open('info/search'); ?>
+        <?php
+          $v = '';
+          if(isset($queryString)) {
+            $v = $queryString;
+          }
+          echo form_input('text', $v, 'class="searchInput"');
+        ?>
+        <?= form_submit('submit', '', 'class="searchSubmit"'); ?>
+      <?= form_close(); ?>
+    </div>
+    <div class="box">
+      <div class="header">ประเภทรถ</div>
+      <div class="list">
+        <ul>
+        </ul>
+      </div>
+    </div>
+    <div class="box">
+      <div class="header">ยี่ห้อรถ</div>
+      <div class="list">
+        <ul>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="infoContentWrapper">
+    <div class="infoContent">
+      <?php foreach($machines as $m) : ?>
+      <div class="sellBox clearfix">
+        <div class="image">
+          <?php if(isset($m->img)) : ?>
+          <div>
+            <img src="<?= base_url() ?>images/uploaded/<?= reset($m->img)->imgname ?>" />
+          </div>
+          <ul class="clearfix">
+            <? foreach($m->img as $i) : ?>
+             <li><img src="<?= base_url() ?>images/uploaded/thumb_120x120/<?= $i->imgname ?>" /></li>
+            <? endforeach; ?>
+          </ul>
+          <?php else : ?>
+          <div class="noImage">ยังไม่มีภาพ</div>
+          <?php endif; ?>
+        </div>
+        <div class="info">
+          <div class="line">
+            <div class="left">ประเภทรถ</div>
+            <div class="right"><?= $m->name ?></div>
+          </div>
+          <div class="line">
+            <div class="left">ยี่ห้อ / รุ่น</div>
+            <div class="right"><?= $m->brand ?></div>
+          </div>
+          <div class="line">
+            <div class="left">Serial No.</div>
+            <div class="right"><?= $m->serial ?></div>
+          </div>
+          <div class="line">
+            <div class="left">จำนวนชั่วโมง</div>
+            <div class="right"><?= $m->hour ?></div>
+          </div>
+          <div class="line">
+            <div class="left">ราคา</div>
+            <div class="right"><?= $m->price ?></div>
+          </div>
+          <div class="line">
+            <div class="left">ข้อมูลอื่น ๆ</div>
+            <div class="right"><?= $m->note ?></div>
+          </div>
+        </div>
+      </div>
+      <?php endforeach; ?>
+      <div class="pagination">
+        <?= $this->pagination->create_links() ?>
+      </div>
+    </div>
+  </div>
+</div>
