447ed59ad0d6118c9866862f8c57d7f313b8e4ec
Phatograph <phatograph@gmail.com>
2011-12-17 18:40:07 +0700

    Backend navigation highlight

--- script/main.js
+++ script/main.js
@@ -1,11 +1,17 @@
 ;$(function() {
   var controllerName = (window.location.pathname.split("/")[2] || 'home'),
+      aControllerName = (window.location.pathname.split("/")[3]),
       mainNavigation = $('.mainNavigation'),
+      adminNav = $('.adminNav'),
       controllerList = ['home', 'info', 'sell', 'rent', 'contactus']
   ;
   
   if(mainNavigation && $.inArray(controllerName, controllerList) !== -1) {
     mainNavigation.find('li.' + controllerName).addClass('selected');
+  }
+  
+  if(adminNav && $.inArray(aControllerName, controllerList) !== -1) {   
+    adminNav.find('li.' + aControllerName).addClass('selected');
   }
   
 });
