1e912efd18e34257da0674af7c8a2f8a794f7528
Phatograph <phatograph@gmail.com>
2011-12-17 15:49:31 +0700

    Fix navigation bug in IE

--- server/main.js
+++ server/main.js
@@ -1,10 +1,10 @@
 ;$(function() {
   var controllerName = (window.location.pathname.split("/")[1] || 'home'),
       mainNavigation = $('.mainNavigation'),
-      controllerList = ['home', 'info', 'sell', 'rent', 'contact']
+      controllerList = ['home', 'info', 'sell', 'rent', 'contactus']
   ;
   
-  if(mainNavigation && $.inArray(controllerName, controllerList)) {
+  if(mainNavigation && $.inArray(controllerName, controllerList) !== -1) {
     mainNavigation.find('li.' + controllerName).addClass('selected');
   }
   
